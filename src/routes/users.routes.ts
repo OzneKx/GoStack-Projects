0e828990ee8f49be7ef63080afff40d3461002b8
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-29 18:24:16 -0300

    GoStack

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // POST http://localhost:3333/users
 usersRouter.post('/', async (request, response) => {
@@ -23,6 +28,16 @@ usersRouter.post('/', async (request, response) => {
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+
+    return response.json({ ok: true });
 });
 
 export default usersRouter;
