e89a0e0a118a5bc3bb6dd2975704fe570fcc962c
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-29 17:00:54 -0300

    GoStack

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+// POST http://localhost:3333/users
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticatedUser = new AuthenticateUserService();
+
+    const { user, token } = await authenticatedUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
