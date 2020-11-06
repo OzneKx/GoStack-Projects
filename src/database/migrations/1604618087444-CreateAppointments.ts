945d5769f0c94213e102a598bc3a3a856174be66
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-11-06 10:26:55 -0300

    GoStack - Projects - Back-end

---
+++ src/database/migrations/1604618087444-CreateAppointments.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1604618087444 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
+ 
